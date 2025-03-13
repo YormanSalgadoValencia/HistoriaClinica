85521ee654f34a54e06f29cae2322fc06e191f5e
JaimeDev <jcardona@stt.com.co>
2025-03-13 15:03:25 -0500

    Avances en la integración

--- src/types/TemplateTypes/Analysis.ts
+++ src/types/TemplateTypes/Analysis.ts
@@ -3,48 +3,23 @@ import type { Concept } from "./Concept";
 export class Analysis {
     idAnalysis: string;
     date: Date;
-    //specialty: Specialty;
     detail: string;
-    concept: Concept;
-    laboratories: Document[];
-    diagnosticImages: Document[];
-    pathology: Document[];
-    molecularTests: Document[];
 
     constructor(
         idAnalysis: string,
         date: Date,
-        //specialty: Specialty,
         detail: string,
-        concept: Concept,
-        laboratories: Document[],
-        diagnosticImages: Document[],
-        pathology: Document[],
-        molecularTests: Document[]
     ) {
         this.idAnalysis = idAnalysis;
         this.date = date;
-        //this.specialty = specialty;
         this.detail = detail;
-        this.concept = concept;
-        this.laboratories = laboratories;
-        this.diagnosticImages = diagnosticImages;
-        this.pathology = pathology;
-        this.molecularTests = molecularTests;
     }
 
-    // Static method to map a JSON to an instance of Analysis
     static fromJSON(data: Analysis): Analysis {
         return new Analysis(
             data.idAnalysis,
             data.date,
-            //data.specialty,
-            data.detail,
-            data.concept,
-            data.laboratories,
-            data.diagnosticImages,
-            data.pathology,
-            data.molecularTests
+            data.detail
         );
     }
 
@@ -52,13 +27,7 @@ export class Analysis {
         return new Analysis(
             analysis.idAnalysis,
             analysis.date,
-            //analysis.specialty,
-            analysis.detail,
-            analysis.concept,
-            analysis.laboratories,
-            analysis.diagnosticImages,
-            analysis.pathology,
-            analysis.molecularTests
+            analysis.detail
         );
     }
 }
